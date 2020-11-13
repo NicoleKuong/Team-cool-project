f9e92ec4cf5396d1edbf74d3fb411dc2d7aefc43
NicoleKuong <nicolekuong@gmail.com>
2020-11-13 18:50:50 +0100

    install dependencies and make nav bar component

---
+++ project-cool/src/Store/store.js
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+//Load Vuex
+Vue.use(Vuex);
+
+//Create store
+export default new Vuex.Store({});
---
+++ project-cool/src/components/NavigationBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="nav-bar">
+    <v-toolbar app dark color="blue-grey darken-1" class="hidden-xs-and-down">
+      <v-toolbar-title>Desktop Menu</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn
+          v-for="item in nav"
+          :key="item.icon"
+          to="#"
+          :title="item.title"
+          flat
+          >{{ item.text }}</v-btn
+        >
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavigationBar",
+};
+</script>
+
+<style scoped></style>>

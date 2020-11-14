c803eca9f563354b4a7116be6c903c14fd17138b
Knibbaz <info@basabbink.nl>
2020-11-14 14:51:06 +0100

    nicole

--- project-cool/src/components/Rankings.vue
+++ project-cool/src/components/Rankings.vue
@@ -7,13 +7,10 @@
           <td><p>Address</p></td>
           <td><p>Recycled plastic (kg)</p></td>
           <td><p>Saved emission (kg)</p></td>
-          <td><p>Visited bins</p></td>
+          <td><p>Recycled items</p></td>
         </tr>
       </thead>
       <tbody>
-        {{
-          createBalanceArray()
-        }}
         <tr>
           <td>1234</td>
           <td>{{ userAddress }}</td>
@@ -21,29 +18,24 @@
           <td>0.1</td>
           <td>1</td>
         </tr>
+      </tbody>
+      <tbody v-for="position in balances" :key="position">
         <tr>
-          <td>1</td>
-          <td>Owner</td>
-          <td>22</td>
-          <td>2</td>
-          <td>4</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>User</td>
-          <td>21</td>
-          <td>2</td>
-          <td>4</td>
+          <td>{{ balances.indexOf(position) + 1 }}</td>
+          <td>{{ position.address }}</td>
+          <td>{{ position.totalWeight }}</td>
+          <td>{{ position.totalEmissionSaved }}</td>
+          <td>{{ position.visitedBins }}</td>
         </tr>
       </tbody>
     </template>
+    {{ createBalanceArray()[0] }}
   </v-simple-table>
 </template>
 
 <script>
 import recyclingHistoryData from "../data/recyclingHistoryData.json";
 import transactionData from "../data/transactionData.json";
-// import plasticTypesData from "../data/plasticTypesData.json";
 
 export default {
   name: "Rankings",
@@ -52,40 +44,76 @@ export default {
       transactionData: transactionData.transactions,
       recyclingHistoryData: recyclingHistoryData.recyclingHistory,
       userAddress: "3PGfXB5bEz7EkbtGMNUYop5aior5X6bUbvL",
-      balances: [
-        { address: "3PGfXB5bEz7EkbtGMNUYop5aior5X6bUbvL", totalWeight: 0.1 },
-        { address: "3PGfXB5bEz7EkbtGMNUYop5aior5X6bUbvL", totalWeight: 0.1 },
+      ranking: [
+        {
+          address: "User1",
+          totalEmissionSaved: 3.6,
+          totalWeight: 0.1,
+          recycleCounter: 2,
+        },
+        {
+          address: "User2",
+          totalEmissionSaved: 1.8,
+          totalWeight: 0.1,
+          recycleCounter: 1,
+        },
       ],
     };
   },
+  computed: {
+    getTotals() {
+      console.log(this.createBalanceArray());
+      return "bye";
+    },
+  },
   methods: {
     createBalanceArray() {
-      for (let i = 0; i < 2; i++) {
-        const { weight, txIds } = this.recyclingHistoryData[i];
-        let address = this.transactionData[txIds[0]].recipient;
+      const emissionSaved = this.recyclingHistoryData.reduce((a, b) => {
+        return a.emissionSaved + b.emissionSaved;
+      });
 
-        let totalWeight = function getTotalWeight(address) {
-          for (let i = 0; i < this.balances.length; i++) {
-            if (this.balances[i].address == address)
-              return (totalWeight = this.balances[i]);
-          }
-        };
+      const weight = this.recyclingHistoryData.reduce((a, b) => {
+        return a.weight + b.weight;
+      });
 
-        totalWeight += weight / 1000;
+      console.log(weight);
+      console.log(emissionSaved);
+      return "Hello";
+      // for (let i = 0; i < this.recyclingHistoryData.length; i++) {
 
-        let account = {
-          address,
-          totalWeight,
-        };
-        console.log(account);
-      }
-    },
-    getRankingUser() {
-      const { weigth, bin, plasticType } = this.recyclingHistoryData[
-        this.recycling
-      ];
-      console.log(weigth, bin, plasticType);
-      console.log(this.recyclingHistoryData[this.recycling]);
+      //     const { bin, weight, txIds, emissionSaved } = this.recyclingHistoryData[
+      //       i
+      //     ];
+
+      //     let totalWeight = 0;
+      //     let totalEmissionSaved = 0;
+      //     let recycleCounter = [];
+
+      //     let address = this.transactionData[txIds[0]].recipient;
+
+      //     //TODO get the totalWeight and totalEmissionSaved
+
+      //     totalWeight = weight / 1000; // devide weight by 1000 to get from grams to kilograms
+      //     totalEmissionSaved += emissionSaved;
+      //     recycleCounter.push(bin);
+
+      //     rankings.push({
+      //       address,
+      //       totalWeight,
+      //       totalEmissionSaved,
+      //       recycleCounter: recycleCounter.length,
+      //     });
+
+      //     this.balances.push({
+      //       address,
+      //       totalWeight,
+      //       totalEmissionSaved,
+      //       recycleCounter: recycleCounter.length,
+      //     });
+      //   }
+      //   console.log(this.balances);
+      //   console.log(rankings);
+      //   return rankings;
     },
   },
 };
@@ -93,7 +121,7 @@ export default {
 
 <style scoped>
 .v-data-table {
-  width: 60%;
+  width: 80%;
   margin: auto;
 }
 </style>

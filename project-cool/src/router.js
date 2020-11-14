3f33b55f7b1b37e89cd74718adee13fa78d714b1
NicoleKuong <nicolekuong@gmail.com>
2020-11-14 15:00:42 +0100

    add user profile

--- project-cool/src/router.js
+++ project-cool/src/router.js
@@ -1,37 +1,28 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import LandingPage from '../src/components/LandingPage.vue';
-import Transactions from '../src/components/Transactions.vue';
+import Vue from "vue";
+import Router from "vue-router";
+import LandingPage from "../src/components/LandingPage.vue";
+import Transactions from "../src/components/Transactions.vue";
+import UserProfile from "../src/components/UserProfile.vue";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'landingPage',
+      path: "/",
+      name: "landingPage",
       component: LandingPage,
     },
     {
-      path: '/transactions',
-      name: 'transactions',
+      path: "/myaccount/transactions",
+      name: "transactions",
       component: Transactions,
     },
-    // {
-    //   path: "/rankings",
-    //   name: "Rankings",
-    //   component: Rankings,
-    // },
-    // {
-    //   path: "/statistics",
-    //   name: "Statistics",
-    //   component: Statistics,
-    // },
-    // {
-    //   path: "/sendtoken",
-    //   name: "Send Token",
-    //   component: SendToken,
-    // },
+    {
+      path: "/myaccount",
+      name: "myaccount",
+      component: UserProfile,
+    },
   ],
-  mode: 'history',
+  mode: "history",
 });

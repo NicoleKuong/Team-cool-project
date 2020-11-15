2d03038e97b9b61a6349bb02c975c743dea44055
Knibbaz <info@basabbink.nl>
2020-11-15 11:14:08 +0100

    added investors

---
+++ project-cool/src/components/InvestorsPage.vue
@@ -0,0 +1,154 @@
+<template>
+  <v-container>
+    <v-container>
+      <center>
+        <h1>Dear investors</h1>
+        <h2>WE NEED YOUR HELP</h2>
+        <p>Tell the investors why we need their help</p>
+      </center>
+    </v-container>
+
+    <v-container>
+      <center>
+        <h1>These are investors that are already helping us out</h1>
+      </center>
+      <v-row>
+        <v-col>
+          <v-card class="pb-3">
+            <v-img src="./../assests/investors/plasticoceans.png"></v-img>
+            <v-card-title class="justify-center">
+              Plastic Oceans International
+            </v-card-title>
+            <v-card-actions>
+              <v-btn class="mx-auto" outlined rounded text>
+                <a href="https://plasticoceans.org/"> Website </a>
+              </v-btn>
+              <v-btn class="mx-auto" outlined rounded text>
+                <a href="mailto:info@plasticoceans.org"> Contact </a>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="pb-3">
+            <v-img src="./../assests/investors/plasticpollutioncoalition.png">
+            </v-img>
+            <v-card-title class="justify-center">
+              Plastic Pollution Coalition
+            </v-card-title>
+            <v-card-actions>
+              <v-btn class="mx-auto" outlined rounded text>
+                <a href="https://www.plasticpollutioncoalition.org/">
+                  Website
+                </a>
+              </v-btn>
+              <v-btn class="mx-auto" outlined rounded text>
+                <a href="mailto:info@plasticpollutioncoalition.org">
+                  Contact
+                </a>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="pb-3">
+            <v-img
+              src="./../assests/investors/ellenmacarthurfoundation.png"
+            ></v-img>
+            <v-card-title class="justify-center">
+              Ellen MacArthur Foundation
+            </v-card-title>
+            <v-card-actions>
+              <v-btn class="mx-auto" outlined rounded text>
+                <a href="https://www.ellenmacarthurfoundation.org/">
+                  Website
+                </a>
+              </v-btn>
+              <v-btn class="mx-auto" outlined rounded text>
+                <a href="mailto:info@ellenmacarthurfoundation.org"> Contact </a>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-card>
+            <v-img src="https://placeimg.com/200/200/asdaefsadf"></v-img>
+            <v-card-title class="justify-center"> Company 1 </v-card-title>
+            <v-card-actions>
+              <v-btn class="mx-auto" outlined rounded text>
+                <a href=""> Website </a>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            <v-img src="https://placeimg.com/200/200/asdfasdfs"></v-img>
+            <v-card-title class="justify-center"> Company 2 </v-card-title>
+            <v-card-actions>
+              <v-btn class="mx-auto" outlined rounded text>
+                <a href=""> Website </a>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            <v-img src="https://placeimg.com/200/200/asdfaeasf"></v-img>
+            <v-card-title class="justify-center"> Company 3 </v-card-title>
+            <v-card-actions>
+              <v-btn class="mx-auto" outlined rounded text>
+                <a href=""> Website </a>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            <v-img src="https://placeimg.com/200/200/asdfasdse"></v-img>
+            <v-card-title class="justify-center"> Company 4</v-card-title>
+            <v-card-actions>
+              <v-btn class="mx-auto" outlined rounded text>
+                <a href=""> Website </a>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            <v-img src="https://placeimg.com/200/200/asfasdfa"></v-img>
+            <v-card-title class="justify-center"> Company 5</v-card-title>
+            <v-card-actions>
+              <v-btn class="mx-auto" outlined rounded text>
+                <a href=""> Website </a>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            <v-img src="https://placeimg.com/200/200/anyhj"></v-img>
+            <v-card-title class="justify-center"> Company 6</v-card-title>
+            <v-card-actions>
+              <v-btn class="mx-auto" outlined rounded text>
+                <a href=""> Website </a>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
--- project-cool/src/router.js
+++ project-cool/src/router.js
@@ -1,40 +1,46 @@
-import Vue from "vue";
-import Router from "vue-router";
-import LandingPage from "../src/components/LandingPage.vue";
-import Transactions from "../src/components/Transactions.vue";
-import Rankings from "../src/components/Rankings.vue";
-import UserProfile from "../src/components/UserProfile.vue";
-import SendToken from "../src/components/SendToken.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import LandingPage from '../src/components/LandingPage.vue';
+import Transactions from '../src/components/Transactions.vue';
+import Rankings from '../src/components/Rankings.vue';
+import UserProfile from '../src/components/UserProfile.vue';
+import SendToken from '../src/components/SendToken.vue';
+import Investors from '../src/components/InvestorsPage.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "landingPage",
+      path: '/',
+      name: 'landingPage',
       component: LandingPage,
     },
     {
-      path: "/ranking",
-      name: "Rankings",
+      path: '/ranking',
+      name: 'Rankings',
       component: Rankings,
     },
     {
-      path: "/myaccount/transactions",
-      name: "transactions",
+      path: '/myaccount/transactions',
+      name: 'transactions',
       component: Transactions,
     },
     {
-      path: "/myaccount",
-      name: "myaccount",
+      path: '/myaccount',
+      name: 'myaccount',
       component: UserProfile,
     },
     {
-      path: "/sendtokens",
-      name: "sendToken",
+      path: '/sendtokens',
+      name: 'sendToken',
       component: SendToken,
     },
+    {
+      path: '/about/investors',
+      name: 'Investors',
+      component: Investors,
+    },
   ],
-  mode: "history",
+  mode: 'history',
 });

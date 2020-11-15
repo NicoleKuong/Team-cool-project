7f03a75f6f6e2994767a25148250fbced41307a5
NicoleKuong <nicolekuong@gmail.com>
2020-11-15 01:28:41 +0100

    set up worldmap

---
+++ project-cool/src/components/WorldMap.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import binsData from "../data/binsData.json";
+
+export default {
+  name: "WorldMap",
+  data() {
+    return {
+      bins: binsData,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
